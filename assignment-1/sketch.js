927efe8d22741e950a56e07f0d51bcb4342c88f8
giraldoelisabeth <giraldo.e@northeastern.edu>
2026-03-18 00:46:44 -0400

    Added final assignment

--- assignment-1/sketch.js
+++ assignment-1/sketch.js
@@ -1,5 +1,4 @@
 function setup() {
-  // Create a canvas as big as the container window
   createCanvas(windowWidth, windowHeight);
   textAlign(CENTER, CENTER);
 }
@@ -7,11 +6,17 @@ function setup() {
 function draw() {
   background(220);
   textSize(height / 16);
-  text("Assignment[1]", width / 2, height / 2);
+  text("Assignment[1]= Frozen Click to open sketch", width / 2, height / 2);
+}
+
+function mousePressed() {
+  window.open(
+    "https://studio.code.org/c/2770701748",
+    "_blank"
+  );
 }
 
 // This function is called any time the window is resized
 function windowResized() {
-  // Change the canvas size to fit the window
   resizeCanvas(windowWidth, windowHeight);
 }
